be47b355f0f6992ca833fed2970e1a04f10ef523
Barbara Phillips <bpincelamattos@gmail.com>
2020-10-27 18:36:22 -0500

    adding search api via typed address

--- scripts/script.js
+++ scripts/script.js
@@ -3,13 +3,14 @@ var service;
 var infowindow;
 var shop = [];
 var shopHTML;
+var geocoder;
 
 function initialize() {
   shopHTML = document.getElementById('shops');
   infowindow = new google.maps.InfoWindow();
   const locationButton = document.getElementById('location');
   
-  const res = axios.post('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDKyAduEhiDy4sefp1ViZ1Ztq7LoEguVt0')
+  axios.post('https://www.googleapiss.com/geolocation/v1/geolocate?key=AIzaSyDKyAduEhiDy4sefp1ViZ1Ztq7LoEguVt0')
     .then(function(response){
       var geolocation = new google.maps.LatLng(response.data.location.lat,response.data.location.lng);
             map = new google.maps.Map(document.getElementById('map'), {
@@ -24,8 +25,53 @@ function initialize() {
       };
       service = new google.maps.places.PlacesService(map);
       service.textSearch(request, callback);
-    }).catch(function(error){
-      console.log(error);
+    }).catch(function(){
+        const us = {center: { lat: 37.1, lng: -95.7 },zoom: 3,}
+        map = new google.maps.Map(document.getElementById("map"), {
+          zoom: us.zoom,
+          center: us.center,
+        });
+        swal("Sorry", "Unable to retrieve your location. Please type your address!", "error");
+        
+        const geocoder = new google.maps.Geocoder();
+        document.getElementById("location").addEventListener("click", () => {
+          geocodeAddress(geocoder, map);
+        });
+        var input = document.getElementById('address');
+        var options = {
+          types: ['address']
+        };
+
+        autocomplete = new google.maps.places.Autocomplete(input, options);
+      
+      function geocodeAddress(geocoder, resultsMap) {
+        var address = document.getElementById('address').value;
+       
+        geocoder.geocode( { 'address': address}, function(results, status) {
+          if (status == 'OK') {
+            axios.get(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${results[0].place_id}&key=AIzaSyDKyAduEhiDy4sefp1ViZ1Ztq7LoEguVt0`)
+              .then(function(response){
+                var geolocation = new google.maps.LatLng(response.data.result.geometry.location.lat,response.data.result.geometry.location.lng);
+                map = new google.maps.Map(document.getElementById('map'), {
+                  center: geolocation,
+                  zoom: 15
+                });
+    
+                var request = {
+                  location: geolocation,
+                  radius: '500',
+                  query: 'tattoo shops'
+                };
+                service = new google.maps.places.PlacesService(map);
+                service.textSearch(request, callback);
+              })
+            
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+        });
+      }
+    
     });
 }
 
@@ -35,9 +81,8 @@ function callback(results, status) {
       var place = currentShop;
       createMarker(currentShop);
       
-      return `
-            <div id="card-wrapper">
-                <div class="card card-style">
+      return `<div id="card-wrapper">
+                <div class="card" style="width: 18rem;">
                   <div class="card-body">
                     <h5 class="card-title">${place.name}</h5>
                     <p class="card-text">${place.formatted_address}</p>
@@ -45,8 +90,7 @@ function callback(results, status) {
                     <a href="#" onclick="saveLocation(${place.geometry.location.lat},${place.geometry.location.lng});initMap()"class="btn btn-dark">Shop Details</a>
                   </div>
                 </div>
-              </div>
-        `
+              </div>`
       })
     map.setCenter(results[0].geometry.location);
     shopHTML.innerHTML = shops.join('');
@@ -54,7 +98,8 @@ function callback(results, status) {
 }
 
 function createMarker(place) {
-    const marker = new google.maps.Marker({map,position: place.geometry.location,});
+    var iconBase = 'images/marker.png';
+    let marker = new google.maps.Marker({map,position: place.geometry.location,icon: iconBase});
     google.maps.event.addListener(marker, "click", () => {
       infowindow.setContent(place.name);
       infowindow.open(map, marker);
